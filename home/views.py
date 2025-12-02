6bbee4ae51cdac38c033fe7c8cea7d1fdb82981d
valdec <valdecssilva@gmail.com>
2025-12-02 20:04:40 -0300

    django1

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Página Principal.")
+
+def sobre(request):
+    return HttpResponse("Sobre o Sistema Django.")
+
+def contato(request):
+    return HttpResponse("Página de Contato.")
+
+def ajuda(request):
+    return HttpResponse("Página de Ajuda.")
--- pweb/settings.py
+++ pweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ts^cybx=krpr*fmty870gzt#4otf13e7xp2w!g0-7wacy5+2jk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
